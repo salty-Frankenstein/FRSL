f566d109c832b7da33507e0617e9285cc6ee4e5c
salty-Frankenstein <1328955419@qq.com>
2018-03-30 23:08:29 +0800

    find_update

--- union_find.h
+++ union_find.h
@@ -9,26 +9,9 @@ struct node{
 
 class union_find{
 	public:
-		void init(int max){
-			set=(struct node *)malloc(sizeof(struct node)*max);
-			t=(int *)malloc(sizeof(int)*max);
-			for(int i=0;i<max;i++){
-			  set[i].father=i;
-			  set[i].data=i;
-			}
-		}
+		void init(int max);
 		
-		int find(int x){
-    		int k=0,i;
-    		while(set[x].father!=x){
-        		k++;
-        		t[k]=x;
-        		x=set[x].father;
-    		}
-    		for(i=1;i<=k-1;i++)
-        		set[t[i]].father=x; 
-    		return x;
-		}
+		int find(int x);
 	
 		void unite(int x,int y){
  	   	set[find(x)].father=find(y);
@@ -51,3 +34,23 @@ class union_find{
 		int *t;
 		struct node *set;
 };
+void union_find::init(int max){
+	set=(struct node *)malloc(sizeof(struct node)*max);
+	t=(int *)malloc(sizeof(int)*max);
+	for(int i=0;i<max;i++){
+		set[i].father=i;
+		set[i].data=i;
+	}
+}
+
+int union_find::find(int x){
+    int k=0,i;
+    while(set[x].father!=x){
+        k++;
+        t[k]=x;
+        x=set[x].father;
+    }
+    for(i=1;i<=k-1;i++)
+        set[t[i]].father=x; 
+    return x;
+}
