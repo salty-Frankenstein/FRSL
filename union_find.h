c37c0305fa812c5c40323692a30cc7c04bfbf40e
salty-Frankenstein <1328955419@qq.com>
2018-03-30 23:03:14 +0800

    find

---
+++ union_find.h
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+
+struct node{
+	int father;
+	int data;
+};
+
+class union_find{
+	public:
+		void init(int max){
+			set=(struct node *)malloc(sizeof(struct node)*max);
+			t=(int *)malloc(sizeof(int)*max);
+			for(int i=0;i<max;i++){
+			  set[i].father=i;
+			  set[i].data=i;
+			}
+		}
+		
+		int find(int x){
+    		int k=0,i;
+    		while(set[x].father!=x){
+        		k++;
+        		t[k]=x;
+        		x=set[x].father;
+    		}
+    		for(i=1;i<=k-1;i++)
+        		set[t[i]].father=x; 
+    		return x;
+		}
+	
+		void unite(int x,int y){
+ 	   	set[find(x)].father=find(y);
+		}
+	
+		bool same_set(int x,int y){
+			return find(x)==find(y);
+		}
+		
+		void set_data(int index,int val){
+			set[index].data=val;
+		}
+		
+		void dispose(){
+			free(set);
+			free(t);
+		}
+	
+	private:
+		int *t;
+		struct node *set;
+};
